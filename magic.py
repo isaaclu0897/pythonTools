7d4b11fa023451c092ff5d05bec8d1875cf83847
Isaac Lu <isaac.lu@wonderland.com.tw>
2020-02-18 14:53:12 +0800

    perf(script/magic): automatic rename file and save magic number into filename

--- magic.py
+++ magic.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python3
 import sys
+import re
 
 fileName = sys.argv[1]
-byte = bytes.fromhex(sys.argv[2])
+newMagic = bytes.fromhex(sys.argv[2])
+length = len(newMagic)
 
 with open(fileName, "rb") as f:
     fileBytes = f.read()
-    fileBytes = byte + fileBytes[len(byte):]
+    oldMagic = fileBytes[:length]
+    fileBytes = newMagic + fileBytes[length:]
 
+fileName = re.sub("\.", "_", sys.argv[1]) + "_" + oldMagic.hex()
 with open(fileName, "wb") as f:
     f.write(fileBytes)
-    
