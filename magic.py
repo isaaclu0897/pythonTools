ef53bc98c34e871360f60311eff85ebe592caf55
Isaac Lu <isaac.lu@wonderland.com.tw>
2020-02-19 20:21:45 +0800

    perf(script/magic): use argparse to wrap parameters

--- magic.py
+++ magic.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python3
-import sys
-import re
 
-fileName = sys.argv[1]
-newMagic = bytes.fromhex(sys.argv[2])
-length = len(newMagic)
+def command_helper():
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description="Magic number change")
+    parser.print_help()
+    
+    parser.add_argument("file", help="input file, ex: tmp.zip")
+    parser.add_argument("magic", help="input magic number, ex: 504b")
+    #parser.add_argument("--method", "-m", help="pack or unpack, defult is pack", default="pack")
+    
+    return parser.parse_args()
 
-with open(fileName, "rb") as f:
-    fileBytes = f.read()
-    oldMagic = fileBytes[:length]
-    fileBytes = newMagic + fileBytes[length:]
 
-fileName = re.sub("\.", "_", sys.argv[1]) + "_" + oldMagic.hex()
-with open(fileName, "wb") as f:
-    f.write(fileBytes)
+if __name__ == '__main__':
+    args = command_helper()
+    
+    import re
+
+    fileName = args.file
+    newMagic = bytes.fromhex(args.magic)
+    length = len(newMagic)
+
+    with open(fileName, "rb") as f:
+        fileBytes = f.read()
+        oldMagic = fileBytes[:length]
+        fileBytes = newMagic + fileBytes[length:]
+
+    fileName = re.sub("\.", "_", fileName) + "_" + oldMagic.hex()
+    with open(fileName, "wb") as f:
+        f.write(fileBytes)
+    
